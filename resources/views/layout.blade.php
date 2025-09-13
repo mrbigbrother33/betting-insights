4fa1133c6f6859cee679a6551a81fb1ea18a31c0
mrbigbrother <morten87andersen@gmail.com>
2025-09-13 11:54:31 +0200

    meta fix

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,7 +8,6 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>{{ $title ?? 'Insights' }}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ $metaTitle ?? $title ?? 'FriIndsigt' }}</title>
