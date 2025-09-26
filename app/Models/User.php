84cc9475cea775f4f6292248c02927cc5a0d73bb
mrbigbrother <morten87andersen@gmail.com>
2025-09-26 13:39:17 +0200

    fix user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,6 +51,6 @@ class User extends Authenticatable
 
     public function isAdmin(): bool
     {
-        return $this->role === 'is_admin';
+        return (bool) ($this->is_admin ?? false);
     }
 }
