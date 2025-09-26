06f5c9ba374d7d031dbad49438a9e249943f23d8
mrbigbrother <morten87andersen@gmail.com>
2025-09-26 09:28:15 +0200

    fix blog clicks migration

---
+++ database/migrations/2025_09_26_072405_fix_blog_clicks_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('blog_clicks', function (Blueprint $table) {
+            $table->dropColumn(['ip', 'visited_on']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('blog_clicks', function (Blueprint $table) {
+            $table->string('ip', 45);
+            $table->date('visited_on');
+        });
+    }
+};
